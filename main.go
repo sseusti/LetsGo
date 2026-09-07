e16e7ab5f2f6be51f04b7a770fdd57a7d3f0134e
sseusti <stepan.s7ep@gmail.com>
2026-09-08 01:09:00 +0300

    create basic server and handler

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
+
+// handler function that`s write ... as a response body
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello from Snippetbox"))
+}
 
 func main() {
-	fmt.Println("Hello world!")
+	// init servemux and bind / to home
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+
+	log.Print("Starting server on :4000")
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
 }
