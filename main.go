1797ac2b0a1f94fb22f7188f21ba77d8a8c8f0c8
sseusti <stepan.s7ep@gmail.com>
2026-09-08 16:49:57 +0300

    add new routs

--- main.go
+++ main.go
@@ -10,10 +10,20 @@ func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello from Snippetbox"))
 }
 
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a specific snippet..."))
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Create a new snippet..."))
+}
+
 func main() {
 	// init servemux and bind / to home
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet/create", snippetCreate)
+	mux.HandleFunc("/snippet/view", snippetView)
 
 	log.Print("Starting server on :4000")
 	err := http.ListenAndServe(":4000", mux)
